c77a5634e79b065274b582db9e2ff04c1cea021d
paradime <brw4824@rit.edu>
2015-08-03 23:13:36 -0400

    Dynamically creates the text file

--- play.rb
+++ play.rb
@@ -3,14 +3,17 @@ require 'open-uri'
 require 'openssl'
 require 'pry'
 
-url = "https://play.google.com/music/preview/pl/AMaBXyny4_cMIcBwuI_l3hgC8UBi__3ZJHLIKuQtTavXSgvv1T0l7wMnLKJZMulH5t72PxkupIxcqjX61xLv4nXMRyVqIB3tlw==?u=0#"
+base_url = "https://play.google.com/music/preview/pl/"
+playlist_id = "AMaBXyny4_cMIcBwuI_l3hgC8UBi__3ZJHLIKuQtTavXSgvv1T0l7wMnLKJZMulH5t72PxkupIxcqjX61xLv4nXMRyVqIB3tlw=="
+url = base_url + playlist_id
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 doc = Nokogiri::HTML(open( url))
 
 row = doc.xpath("//tr")
 output = ''
 data_vals = row.xpath("//td")
-out_file = File.new("alternative-rock.txt", 'w')
+title = doc.xpath("//div[@class='title fade-out']").text.tr_s(' ', '-')
+out_file = File.new("#{title}.txt", 'w')
 data_vals.each_with_index do |data, index|
     if data.text =~ /^\d+$/
         output += data
